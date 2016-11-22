6528029f5312ef7161f1bc8e008e181447f64f15
Frank Jiang <ffjiang@princeton.edu>
2016-11-22 07:21:04 +0000

    merging

--- trace/trace.py
+++ trace/trace.py
@@ -54,7 +54,7 @@ def create_simple_network(inpt, out, learning_rate=0.001):
 
         # layer 1 - original stride 1
         W_conv1 = weight_variable('W_conv1', [FOV, FOV, 1, 2])
-        b_conv1 = unbiased_bias_variable([2])
+        b_conv1 = bias_variable([2])
         prediction = conv2d(image, W_conv1, dilation=1) + b_conv1
 
         w1_hist = tf.histogram_summary('W_conv1 weights', W_conv1)
@@ -99,7 +99,7 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 1 - original stride 1
         W_conv1 = weight_variable('W_conv1', [4, 4, 1, 48])
-        b_conv1 = unbiased_bias_variable([48])
+        b_conv1 = bias_variable([48])
         h_conv1 = tf.nn.relu(conv2d(image, W_conv1, dilation=1) + b_conv1)
 
         w1_hist = tf.histogram_summary('W_conv1 weights', W_conv1)
@@ -126,7 +126,7 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 3 - original stride 1
         W_conv2 = weight_variable('W_conv2', [5, 5, 48, 48])
-        b_conv2 = unbiased_bias_variable([48])
+        b_conv2 = bias_variable([48])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, dilation=2) + b_conv2)
 
         w2_hist = tf.histogram_summary('W_conv2 weights', W_conv2)
@@ -153,7 +153,7 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 5 - original stride 1
         W_conv3 = weight_variable('W_conv3', [5, 5, 48, 48])
-        b_conv3 = unbiased_bias_variable([48])
+        b_conv3 = bias_variable([48])
         h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, dilation=4) + b_conv3)
 
         w3_hist = tf.histogram_summary('W_conv3 weights', W_conv3)
@@ -180,7 +180,7 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 7 - original stride 1
         W_conv4 = weight_variable('W_conv4', [4, 4, 48, 48])
-        b_conv4 = unbiased_bias_variable([48])
+        b_conv4 = bias_variable([48])
         h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4, dilation=8) + b_conv4)
 
         w4_hist = tf.histogram_summary('W_conv4 weights', W_conv4)
@@ -208,12 +208,26 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 9 - original stride 1
         W_fc1 = weight_variable('W_fc1', [4, 4, 48, 200])
-        b_fc1 = unbiased_bias_variable([200])
+        b_fc1 = bias_variable([200])
         h_fc1 = tf.nn.relu(conv2d(h_pool4, W_fc1, dilation=16) + b_fc1)
 
         w_fc1_hist = tf.histogram_summary('W_fc1 weights', W_fc1)
         b_fc1_hist = tf.histogram_summary('b_fc1 biases', b_fc1)
         h_fc1_hist = tf.histogram_summary('h_fc1 activations', h_fc1)
+
+        # Compute image summaries of the 48 feature maps
+        cx = 10
+        cy = 20
+        iy = 151
+        ix = 151
+        h_fc1_packed = tf.reshape(h_fc1, (iy, ix, 200))
+        iy += 4
+        ix += 4
+        h_fc1_packed = tf.image.resize_image_with_crop_or_pad(h_fc1_packed, iy, ix)
+        h_fc1_packed = tf.reshape(h_fc1_packed, (iy, ix, cy, cx))
+        h_fc1_packed = tf.transpose(h_fc1_packed, (2,0,3,1)) # cy, iy,  cx, ix
+        h_fc1_packed = tf.reshape(h_fc1_packed, (1, cy * iy, cx * ix, 1))
+        h_fc1_image_summary = tf.image_summary('Layer fc1 activations', h_fc1_packed)
 
 
         # layer 10 - original stride 2
@@ -261,7 +275,8 @@ def create_network(inpt, out, learning_rate=0.0001):
                                              h_conv1_image_summary,
                                              h_conv2_image_summary,
                                              h_conv3_image_summary,
-                                             h_conv4_image_summary
+                                             h_conv4_image_summary,
+                                             h_fc1_image_summary
                                              ])
 
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
@@ -277,7 +292,7 @@ def train(n_iterations=200000):
     print ('Run tensorboard to visualize training progress')
     with tf.Session() as sess:
         summary_writer = tf.train.SummaryWriter(
-                       snemi3d.folder()+'tmp/FOV115_OUTPT151_bias0/', graph=sess.graph)
+                       snemi3d.folder()+'tmp/FOV115_OUTPT151_bias0.1/', graph=sess.graph)
 
         sess.run(tf.initialize_all_variables())
         for step, (inputs, affinities) in enumerate(batch_iterator(FOV,OUTPT,INPT)):
@@ -301,7 +316,7 @@ def train(n_iterations=200000):
                 summary_writer.add_summary(image_summary, step)
 
                 # Save the variables to disk.
-                save_path = net.saver.save(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_bias0/model.ckpt")
+                save_path = net.saver.save(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_bias0.1/model.ckpt")
                 print("Model saved in file: %s" % save_path)
 
             if step == n_iterations:
@@ -320,7 +335,7 @@ def predict():
             net = create_network(inputShape, outputShape)
             with tf.Session() as sess:
                 # Restore variables from disk.
-                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_bias0/model.ckpt")
+                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_bias0.1/model.ckpt")
                 print("Model restored.")
 
                 #TODO pad the image with zeros so that the ouput covers the whole dataset
