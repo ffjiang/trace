18a08b8b429dccba07162e3d840d2b8ba66a24cb
Frank Jiang <ffjiang@princeton.edu>
2016-11-23 20:34:44 +0000

    added validation summary

--- trace/trace.py
+++ trace/trace.py
@@ -27,13 +27,13 @@ def weight_variable(name, shape):
   #initial = tf.truncated_normal(shape, stddev=0.05)
   return tf.get_variable(name, shape=shape, initializer=tf.contrib.layers.xavier_initializer(uniform=False))
 
-def bias_variable(shape):
+def bias_variable(name, shape):
   initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+  return tf.get_variable(name, initializer=initial)
 
-def unbiased_bias_variable(shape):
+def unbiased_bias_variable(name, shape):
   initial = tf.constant(0.0, shape=shape)
-  return tf.Variable(initial)
+  return tf.get_variable(name, initializer=initial)
 
 def conv2d(x, W, dilation=None):
   return tf.nn.convolution(x, W, strides=[1, 1], padding='VALID', dilation_rate= [dilation, dilation])
@@ -54,7 +54,7 @@ def create_simple_network(inpt, out, learning_rate=0.001):
 
         # layer 1 - original stride 1
         W_conv1 = weight_variable('W_conv1', [FOV, FOV, 1, 2])
-        b_conv1 = unbiased_bias_variable([2])
+        b_conv1 = unbiased_bias_variable('b_conv1', [2])
         prediction = conv2d(image, W_conv1, dilation=1) + b_conv1
 
         w1_hist = tf.histogram_summary('W_conv1 weights', W_conv1)
@@ -90,8 +90,8 @@ def create_simple_network(inpt, out, learning_rate=0.001):
 def create_network(inpt, out, learning_rate=0.0001):
     class Net:
         # layer 0
-        image = tf.placeholder(tf.float32, shape=[1, inpt, inpt, 1])
-        target = tf.placeholder(tf.float32, shape=[1, out, out, 2])
+        image = tf.placeholder(tf.float32, shape=[None, inpt, inpt, 1])
+        target = tf.placeholder(tf.float32, shape=[None, out, out, 2])
         input_summary = tf.image_summary('input image', image)
         output_patch_summary = tf.image_summary('output patch', image[:,FOV//2:FOV//2+out,FOV//2:FOV//2+out,:])
         target_x_summary = tf.image_summary('target x affinities', target[:,:,:,:1])
@@ -99,8 +99,8 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 1 - original stride 1
         W_conv1 = weight_variable('W_conv1', [4, 4, 1, 96])
-        b_conv1 = bias_variable([96])
-        h_conv1 = tf.nn.relu(conv2d(image, W_conv1, dilation=1) + b_conv1)
+        b_conv1 = unbiased_bias_variable('b_conv1', [96])
+        h_conv1 = tf.nn.elu(conv2d(image, W_conv1, dilation=1) + b_conv1)
 
         w1_hist = tf.histogram_summary('W_conv1 weights', W_conv1)
         b1_hist = tf.histogram_summary('b_conv1 biases', b_conv1)
@@ -111,7 +111,7 @@ def create_network(inpt, out, learning_rate=0.0001):
         cy = 12
         iy = inpt - 3
         ix = iy
-        h_conv1_packed = tf.reshape(h_conv1, (iy, ix, 96))
+        h_conv1_packed = tf.reshape(h_conv1[0], (iy, ix, 96))
         iy += 4
         ix += 4
         h_conv1_packed = tf.image.resize_image_with_crop_or_pad(h_conv1_packed, iy, ix)
@@ -130,8 +130,8 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 3 - original stride 1
         W_conv2 = weight_variable('W_conv2', [5, 5, 96, 96])
-        b_conv2 = bias_variable([96])
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, dilation=2) + b_conv2)
+        b_conv2 = unbiased_bias_variable('b_conv2', [96])
+        h_conv2 = tf.nn.elu(conv2d(h_pool1, W_conv2, dilation=2) + b_conv2)
 
         w2_hist = tf.histogram_summary('W_conv2 weights', W_conv2)
         b2_hist = tf.histogram_summary('b_conv2 biases', b_conv2)
@@ -142,7 +142,7 @@ def create_network(inpt, out, learning_rate=0.0001):
         cy = 12
         iy = inpt - 3 - 1 - (2 * 4)
         ix = iy
-        h_conv2_packed = tf.reshape(h_conv2, (iy, ix, 96))
+        h_conv2_packed = tf.reshape(h_conv2[0], (iy, ix, 96))
         iy += 4
         ix += 4
         h_conv2_packed = tf.image.resize_image_with_crop_or_pad(h_conv2_packed, iy, ix)
@@ -161,8 +161,8 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 5 - original stride 1
         W_conv3 = weight_variable('W_conv3', [5, 5, 96, 96])
-        b_conv3 = bias_variable([96])
-        h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, dilation=4) + b_conv3)
+        b_conv3 = unbiased_bias_variable('b_conv3', [96])
+        h_conv3 = tf.nn.elu(conv2d(h_pool2, W_conv3, dilation=4) + b_conv3)
 
         w3_hist = tf.histogram_summary('W_conv3 weights', W_conv3)
         b3_hist = tf.histogram_summary('b_conv3 biases', b_conv3)
@@ -173,7 +173,7 @@ def create_network(inpt, out, learning_rate=0.0001):
         cy = 12
         iy = inpt - 3 - 1 - (2 * 4) - (2 * 1) - (4 * 4)
         ix = iy
-        h_conv3_packed = tf.reshape(h_conv3, (iy, ix, 96))
+        h_conv3_packed = tf.reshape(h_conv3[0], (iy, ix, 96))
         iy += 4
         ix += 4
         h_conv3_packed = tf.image.resize_image_with_crop_or_pad(h_conv3_packed, iy, ix)
@@ -191,8 +191,8 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 7 - original stride 1
         W_conv4 = weight_variable('W_conv4', [4, 4, 96, 96])
-        b_conv4 = bias_variable([96])
-        h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4, dilation=8) + b_conv4)
+        b_conv4 = unbiased_bias_variable('b_conv4', [96])
+        h_conv4 = tf.nn.elu(conv2d(h_pool3, W_conv4, dilation=8) + b_conv4)
 
         w4_hist = tf.histogram_summary('W_conv4 weights', W_conv4)
         b4_hist = tf.histogram_summary('b_conv4 biases', b_conv4)
@@ -203,7 +203,7 @@ def create_network(inpt, out, learning_rate=0.0001):
         cy = 12
         iy = inpt - 3 - 1 - (2 * 4) - (2 * 1) - (4 * 4) - (4 * 1) - (8 * 3)
         ix = iy
-        h_conv4_packed = tf.reshape(h_conv4, (iy, ix, 96))
+        h_conv4_packed = tf.reshape(h_conv4[0], (iy, ix, 96))
         iy += 4
         ix += 4
         h_conv4_packed = tf.image.resize_image_with_crop_or_pad(h_conv4_packed, iy, ix)
@@ -219,8 +219,8 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 9 - original stride 1
         W_fc1 = weight_variable('W_fc1', [4, 4, 96, 200])
-        b_fc1 = bias_variable([200])
-        h_fc1 = tf.nn.relu(conv2d(h_pool4, W_fc1, dilation=16) + b_fc1)
+        b_fc1 = unbiased_bias_variable('b_fc1', [200])
+        h_fc1 = tf.nn.elu(conv2d(h_pool4, W_fc1, dilation=16) + b_fc1)
 
         w_fc1_hist = tf.histogram_summary('W_fc1 weights', W_fc1)
         b_fc1_hist = tf.histogram_summary('b_fc1 biases', b_fc1)
@@ -231,7 +231,7 @@ def create_network(inpt, out, learning_rate=0.0001):
         cy = 20
         iy = out
         ix = out
-        h_fc1_packed = tf.reshape(h_fc1, (iy, ix, 200))
+        h_fc1_packed = tf.reshape(h_fc1[0], (iy, ix, 200))
         iy += 4
         ix += 4
         h_fc1_packed = tf.image.resize_image_with_crop_or_pad(h_fc1_packed, iy, ix)
@@ -243,7 +243,7 @@ def create_network(inpt, out, learning_rate=0.0001):
 
         # layer 10 - original stride 2
         W_fc2 = weight_variable('W_fc2', [1, 1, 200, 2])
-        b_fc2 = unbiased_bias_variable([2])
+        b_fc2 = unbiased_bias_variable('b_fc2', [2])
         prediction = conv2d(h_fc1, W_fc2, dilation=16) + b_fc2
 
         w_fc2_hist = tf.histogram_summary('W_fc2 weights', W_fc2)
@@ -298,40 +298,67 @@ def create_network(inpt, out, learning_rate=0.0001):
     return Net()
 
 def train(n_iterations=200000):
+    with h5py.File(snemi3d.folder()+'validation-input.h5','r') as validation_input_file:
+        validation_input = validation_input_file['main'][:5,:,:].astype(np.float32) / 255.0
+        num_validation_layers = validation_input.shape[0]
+        validation_input_shape = validation_input.shape[1]
+        validation_output_shape = validation_input.shape[1] - FOV + 1
+        with h5py.File(snemi3d.folder()+'validation-affinities.h5','r') as validation_label_file:
+            validation_labels = validation_label_file['main'][:,:5,:,:]
 
-    net = create_network(INPT, OUTPT)
-    print ('Run tensorboard to visualize training progress')
-    with tf.Session() as sess:
-        summary_writer = tf.train.SummaryWriter(
-                       snemi3d.folder()+'tmp/FOV115_OUTPT151_96map/', graph=sess.graph)
+            with tf.variable_scope('foo'):
+                net = create_network(INPT, OUTPT)
+            with tf.variable_scope('foo', reuse=True):
+                validation_net = create_network(validation_input_shape, validation_output_shape)
 
-        sess.run(tf.initialize_all_variables())
-        for step, (inputs, affinities) in enumerate(batch_iterator(FOV,OUTPT,INPT)):
-            sess.run(net.train_step, 
-                    feed_dict={net.image: inputs,
-                               net.target: affinities})
+            print ('Run tensorboard to visualize training progress')
+            with tf.Session() as sess:
+                summary_writer = tf.train.SummaryWriter(
+                               snemi3d.folder()+'tmp/FOV115_OUTPT151_96map_val/', graph=sess.graph)
 
-            if step % 10 == 0:
-                print ('step :'+str(step))
-                summary = sess.run(net.summary_op,
-                    feed_dict={net.image: inputs,
-                               net.target: affinities})
-            
-                summary_writer.add_summary(summary, step)
+                sess.run(tf.global_variables_initializer())
+                for step, (inputs, affinities) in enumerate(batch_iterator(FOV,OUTPT,INPT)):
+                    sess.run(net.train_step, 
+                            feed_dict={net.image: inputs,
+                                       net.target: affinities})
 
-            if step % 1000 == 0:
-                image_summary = sess.run(net.image_summary_op,
-                    feed_dict={net.image: inputs,
-                               net.target: affinities})
-            
-                summary_writer.add_summary(image_summary, step)
+                    if step % 10 == 0:
+                        print ('step :'+str(step))
+                        summary = sess.run(net.summary_op,
+                            feed_dict={net.image: inputs,
+                                       net.target: affinities})
+                    
+                        summary_writer.add_summary(summary, step)
 
-                # Save the variables to disk.
-                save_path = net.saver.save(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map/model.ckpt")
-                print("Model saved in file: %s" % save_path)
+                    if step % 100 == 0:
+                        image_summary = sess.run(net.image_summary_op,
+                            feed_dict={net.image: inputs,
+                                       net.target: affinities})
+                    
+                        summary_writer.add_summary(image_summary, step)
 
-            if step == n_iterations:
-                break
+                        # Save the variables to disk.
+                        save_path = net.saver.save(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map_val/model.ckpt")
+                        print("Model saved in file: %s" % save_path)
+
+                        # Measure validation error
+
+                        #TODO pad the image with zeros so that the ouput covers the whole dataset
+                        totalPixelError = 0.0
+                        reshaped_label = np.einsum('dzyx->zyxd', 
+                                validation_labels[0:2,:,
+                                    FOV//2:FOV//2+validation_output_shape,
+                                    FOV//2:FOV//2+validation_output_shape])
+
+                        pixel_error_summary = sess.run(validation_net.pixel_error_summary,
+                                feed_dict={validation_net.image: 
+                                        validation_input.reshape(num_validation_layers, validation_input_shape, validation_input_shape, 1),
+                                    validation_net.target: reshaped_label})
+
+                        summary_writer.add_summary(pixel_error_summary, step)
+
+                    if step == n_iterations:
+                        break
 
 def evaluate(dataset):
     from tqdm import tqdm
@@ -345,12 +372,12 @@ def evaluate(dataset):
             net = create_network(inputShape, outputShape)
             with tf.Session() as sess:
                 # Restore variables from disk.
-                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map/model.ckpt")
+                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map_val/model.ckpt")
                 print("Model restored.")
 
                 #TODO pad the image with zeros so that the ouput covers the whole dataset
                 totalPixelError = 0.0
-                for z in xrange(inpt.shape[0]):
+                for z in tqdm(xrange(inpt.shape[0])):
                     print ('z: {} of {}'.format(z,inpt.shape[0]))
                     reshapedLabel = np.einsum('dzyx->zyxd', labels[0:2,z:z+1,FOV//2:FOV//2+outputShape,FOV//2:FOV//2+outputShape])
 
@@ -377,7 +404,7 @@ def predict():
             net = create_network(inputShape, outputShape)
             with tf.Session() as sess:
                 # Restore variables from disk.
-                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map/model.ckpt")
+                net.saver.restore(sess, snemi3d.folder()+"tmp/FOV115_OUTPT151_96map_val/model.ckpt")
                 print("Model restored.")
 
                 #TODO pad the image with zeros so that the ouput covers the whole dataset
